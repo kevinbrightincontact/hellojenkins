2f83a6a8fdaf326942fd2716fad60e9c58517dce
Kevin Bright <kevin.bright@niceincontact.com>
2021-01-19 14:08:09 -0700

    b

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/inContact/hellojenkins
 
 go 1.14
+
+require (
+	github.com/stretchr/testify v1.6.1
+	go.mongodb.org/mongo-driver v1.4.5
+)
--- mypackage/mypackage.go
+++ mypackage/mypackage.go
@@ -1,5 +1,8 @@
 package mypackage
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 func someFunc() int {
+	primitive.ObjectIDFromHex("12345667")
 	return 1
 }
